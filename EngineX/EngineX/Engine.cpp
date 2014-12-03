2f4fc12865695032ecf691a08d720879383de5a8
Peter Bergman <peter.bergman@live.se>
2014-12-03 20:29:57 +0100

    Created basic event loop in Engine class

--- EngineX/EngineX/Engine.cpp
+++ EngineX/EngineX/Engine.cpp
@@ -1,1 +1,16 @@
 #include "Engine.h"
+
+void Engine::run() {
+    while (is_running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    is_running = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
--- EngineX/EngineX/Engine.h
+++ EngineX/EngineX/Engine.h
@@ -2,8 +2,14 @@
 #define __EngineX__Engine__
 
 #include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#include <SDL2_ttf/SDL_ttf.h>
+
 class Engine {
 public:
     void run();
+private:
+    bool is_running;
 };
 #endif
