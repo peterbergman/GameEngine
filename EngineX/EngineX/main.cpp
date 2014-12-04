a5940af852b61dc9247eb0aa1a3501e0345ed25b
Peter Bergman <peter.bergman@live.se>
2014-12-04 10:17:18 +0100

    Removed namespace

--- EngineX/EngineX/main.cpp
+++ EngineX/EngineX/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include "Engine.h"
 
-using namespace enginex;
-
 int main(int argc, const char * argv[]) {
     Engine* game_engine = new Engine("My Game");
     delete game_engine;
