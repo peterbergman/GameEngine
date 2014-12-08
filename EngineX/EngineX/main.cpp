6b14c35182ce5c0fa26ce8069c079c9427928ba8
Peter Bergman <peter.bergman@live.se>
2014-12-08 13:17:29 +0100

    Change in application

--- EngineX/EngineX/main.cpp
+++ EngineX/EngineX/main.cpp
@@ -3,19 +3,34 @@
 
 using namespace std;
 
-void Print(SDL_Event event) {
+Sprite* sprite = new ImageSprite("/Users/Peter/Desktop/include.png", 30, 30, 80, 163);
+
+void SpriteListener(SDL_Event event) {
     if (event.type == SDL_MOUSEBUTTONDOWN) {
-        cout << "clicked!\n";
+        cout << "click!\n";
     } else if (event.type == SDL_KEYDOWN) {
-        cout << "key down!\n";
+        switch (event.key.keysym.sym) {
+            case SDLK_UP:
+                sprite->SetY(sprite->GetY() - 10);
+                break;
+            case SDLK_DOWN:
+                sprite->SetY(sprite->GetY() + 10);
+                break;
+            case SDLK_LEFT:
+                sprite->SetX(sprite->GetX() - 10);
+                break;
+            case SDLK_RIGHT:
+                sprite->SetX(sprite->GetX() + 10);
+                break;
+            default:
+                break;
+        }
     }
 }
 
 int main(int argc, const char * argv[]) {
-    
     Engine* game_engine = new Engine("My Game");
-    Sprite* sprite = new ImageSprite("/Users/Peter/Desktop/include.png", 30, 30, 80, 163);
-    sprite->AddActionListener(Print);
+    sprite->AddActionListener(SpriteListener);
     game_engine->AddSprite(sprite);
     game_engine->Run();
     delete game_engine;
