f45f5140421b54e7994960e615f60434318d386a
Peter Bergman <peter.bergman@live.se>
2014-12-10 08:49:26 +0100

    Added custom collision listener

--- EngineX/EngineX/main.cpp
+++ EngineX/EngineX/main.cpp
@@ -3,11 +3,17 @@
 
 using namespace std;
 
+Engine* game_engine = new Engine("My Game", 20);
 Sprite* sprite1 = new ImageSprite("/Users/Peter/Desktop/include.png", 30, 30, 80, 163);
 Sprite* sprite2 = new ImageSprite("/Users/Peter/Desktop/include.png", 500, 30, 80, 163);
 
+void CollisionListener(Sprite* sprite1, Sprite* sprite2) {
+    cout << "Collision detected!\n";
+    game_engine->RemoveSprite(sprite1);
+    game_engine->RemoveSprite(sprite2);
+}
 
-void SpriteL1istener(SDL_Event event) {
+void SpriteL1istener(SDL_Event event) { // TODO: should maybe get the sprite as well as the event?
     if (event.type == SDL_MOUSEBUTTONDOWN) {
         cout << "click!\n";
     } else if (event.type == SDL_KEYDOWN) {
@@ -39,14 +45,14 @@ void Sprite2TimeListener(SDL_Event event) {
 }
 
 int main(int argc, const char * argv[]) {
-    Engine* game_engine = new Engine("My Game", 20);
-    
     sprite1->AddActionListener(SpriteL1istener);
     sprite1->AddTimeEventListener(Sprite1TimeListener, 50);
     game_engine->AddSprite(sprite1);
     
     sprite2->AddTimeEventListener(Sprite2TimeListener, 50);
     game_engine->AddSprite(sprite2);
+    
+    game_engine->SetCollisionListener(CollisionListener);
     
     game_engine->Run();
     delete game_engine;
