2d2401af24d0c385b06a1b17da37bd1982042865
Peter Bergman <peter.bergman@live.se>
2014-12-29 16:47:24 +0100

    Added possibility to specify different images that the animated sprite should switch between and a delay between switches as well

--- EngineX/EngineX/AnimatedSprite.h
+++ EngineX/EngineX/AnimatedSprite.h
@@ -2,19 +2,23 @@
 #define __EngineX__AnimatedSprite__
 
 #include <string>
+#include <vector>
 #include <SDL2/SDL.h>
 #include <SDL2_image/SDL_image.h>
 #include "Sprite.h"
 
 class AnimatedSprite : public Sprite {
 public:
-    static AnimatedSprite* GetInstance(std::string, int, int, int, int);
-    virtual void Draw();
+    static AnimatedSprite* GetInstance(std::vector<std::string>, int, int, int, int, int);
+    virtual void Draw(int);
     virtual ~AnimatedSprite();
 private:
-    AnimatedSprite(std::string, int, int, int, int); // Guard against value semantic
+    AnimatedSprite(std::vector<std::string>, int, int, int, int, int); // Guard against value semantic
     AnimatedSprite(const AnimatedSprite&); // Guard against value semantic
     const AnimatedSprite& operator=(const AnimatedSprite&); // Guard against value semantic
+    std::vector<std::string> images;
+    int image_change_delay, image_index;
+    double time_since_last_draw;
 };
 
 #endif
