f6425cf6b9d20253d8d5895df2f06b8d388ad4b9
Peter Bergman <peter.bergman@live.se>
2014-12-30 22:55:42 +0100

    Added level class

---
+++ GameEngine/GameEngine/Level.cpp
@@ -0,0 +1,9 @@
+//
+//  Level.cpp
+//  GameEngine
+//
+//  Created by Peter Bergman on 30/12/14.
+//  Copyright (c) 2014 Peter Bergman. All rights reserved.
+//
+
+#include "Level.h"
---
+++ GameEngine/GameEngine/Level.h
@@ -0,0 +1,14 @@
+//
+//  Level.h
+//  GameEngine
+//
+//  Created by Peter Bergman on 30/12/14.
+//  Copyright (c) 2014 Peter Bergman. All rights reserved.
+//
+
+#ifndef __GameEngine__Level__
+#define __GameEngine__Level__
+
+#include <stdio.h>
+
+#endif /* defined(__GameEngine__Level__) */
