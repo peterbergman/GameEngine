670f58ee29af38a01b09ee0b2b5c74e01bb13218
Peter Bergman <peter.bergman@live.se>
2014-12-30 22:55:51 +0100

    Added Level class

--- GameEngine/GameEngine/Level.cpp
+++ GameEngine/GameEngine/Level.cpp
@@ -1,9 +1,63 @@
-//
-//  Level.cpp
-//  GameEngine
-//
-//  Created by Peter Bergman on 30/12/14.
-//  Copyright (c) 2014 Peter Bergman. All rights reserved.
-//
-
 #include "Level.h"
+#include "Window.h"
+
+Level::Level() {
+    
+}
+
+// Adds a new sprite to the internal window by taking in a sprite pointer as argument.
+// First sends the renderer for the window to the sprite since the sprite needs it in order to draw itself.
+// When the sprite has access to the render, it can create its texture. This is done here by calling Sprite::SetUpTexture.
+// After these steps, the sprite can be added to the vector of sprites which will be rendererd during the next iteration of the main event loop.
+void Level::AddSprite(Sprite* sprite) {
+    sprites.push_back(sprite);
+    if (is_loaded) {
+        window->LoadSprite(sprite);
+    }
+}
+
+// Removes an existing sprite from the window by taking in a sprite pointer as argument.
+// Both frees the memory allocated by the sprite object and removes it from the vector of sprites.
+void Level::RemoveSprite(Sprite* sprite) {
+    delete sprite;
+    for (int i = 0; i < sprites.size(); i++) {
+        if (sprite == sprites[i]) {
+            sprites.erase(sprites.begin() + i);
+        }
+    }
+}
+
+// Returns a vector of all sprites that have been added to the window.
+std::vector<Sprite*> Level::GetSprites() {
+    return sprites;
+}
+
+// Sets the background of the level by loading the image located at the the path specified as argument.
+// The background is added to the level as a new StaticSprite which is then by calling Window::AddSprite.
+void Level::SetBackground(std::string background_image_path) {
+    Sprite* background_sprite = StaticSprite::GetInstance(background_image_path, 0, 0, 0, 0);
+    AddSprite(background_sprite);
+}
+
+// Sets the loaded flag of this level.
+void Level::SetLoaded(bool is_loaded) {
+    this->is_loaded = is_loaded;
+}
+
+// Sets the window pointer needed to load more sprites is new ones are added after the level is loaded.
+void Level::SetWindow(Window* window) {
+    this->window = window;
+}
+
+// Delegates an event to the sprites that have been added to the level.
+void Level::PropagateEventToSprites(SDL_Event event) {
+    for (Sprite* sprite : sprites) {
+        sprite->DelegateEvent(event);
+    }
+}
+
+Level::~Level() {
+    for (Sprite* sprite : sprites) {
+        delete sprite;
+    }
+}
--- GameEngine/GameEngine/Level.h
+++ GameEngine/GameEngine/Level.h
@@ -1,14 +1,51 @@
-//
-//  Level.h
-//  GameEngine
-//
-//  Created by Peter Bergman on 30/12/14.
-//  Copyright (c) 2014 Peter Bergman. All rights reserved.
-//
-
 #ifndef __GameEngine__Level__
 #define __GameEngine__Level__
 
-#include <stdio.h>
+#include "Sprite.h"
+#include "StaticSprite.h"
+//#include "Window.h"
 
-#endif /* defined(__GameEngine__Level__) */
+class Window;
+
+class Level {
+public:
+    
+    Level();
+    
+    // Adds a new sprite to this level by taking in a sprite pointer as argument.
+    void AddSprite(Sprite*); // TODO: implement layers? Could maybe be done with a tree set to hold the sprites instead of a vector
+    
+    // Removes an existing sprite from this level by taking in a sprite pointer as argument.
+    // Frees any memory associated with the sprite object.
+    void RemoveSprite(Sprite*);
+    
+    // Returns a vector of all sprites that have been added to the level.
+    std::vector<Sprite*> GetSprites();
+    
+    // Sets the background of the level by loading the image located at the the path specified as argument.
+    void SetBackground(std::string);
+    
+    // Sets the loaded flag of this level.
+    void SetLoaded(bool);
+    
+    // Sets the window pointer needed to load more sprites is new ones are added after the level is loaded.
+    void SetWindow(Window*);
+    
+    // Delegates an event to the sprites that have been added to the level.
+    void PropagateEventToSprites(SDL_Event);
+    
+    ~Level();
+    
+private:
+    
+    // A vector that contains all sprites that have been added to this level.
+    std::vector<Sprite*> sprites;
+    
+    // A flag to indiciate if this level is currently loaded or not
+    bool is_loaded;
+    
+    // A pointer to the window that this level is loaded in.
+    Window* window;
+};
+
+#endif
