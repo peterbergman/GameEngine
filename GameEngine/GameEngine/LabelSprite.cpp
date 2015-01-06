7704e4ef2a98443fbd9d3529c95507baf9f3aabf
Peter Bergman <peter.bergman@live.se>
2015-01-06 23:14:27 +0100

    Added a label sprite

---
+++ GameEngine/GameEngine/LabelSprite.cpp
@@ -0,0 +1,26 @@
+#include "LabelSprite.h"
+
+LabelSprite* LabelSprite::GetInstance(std::string tag, std::string message, int x_pos, int y_pos) {
+    return new LabelSprite(tag, message, x_pos, y_pos);
+}
+
+LabelSprite::LabelSprite(std::string tag, std::string message, int x_pos, int y_pos):message(message), Sprite(tag, x_pos, y_pos, 25 * message.length(), 50, "") {
+    font = TTF_OpenFont("/Users/Peter/Documents/DSV/Prog3/images/gameengine/font.ttf", 48);
+    TTF_SetFontOutline(font, 1);
+}
+
+void LabelSprite::Draw(int elapsed_time) {
+    SDL_DestroyTexture(texture);
+    SDL_Color white = { 255, 255, 255 };
+    SDL_Surface* message_surface = TTF_RenderText_Solid(font, message.c_str(), white);
+    texture = SDL_CreateTextureFromSurface(renderer, message_surface);
+    SDL_FreeSurface(message_surface);
+    SDL_RenderCopy(renderer, texture, NULL, boundary);
+}
+
+LabelSprite::~LabelSprite() {
+    delete boundary;
+    TTF_CloseFont(font);
+    SDL_DestroyTexture(texture);
+}
+
---
+++ GameEngine/GameEngine/LabelSprite.h
@@ -0,0 +1,37 @@
+#ifndef __GameEngine__LabelSprite__
+#define __GameEngine__LabelSprite__
+
+#include <string>
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#include <SDL2_ttf/SDL_ttf.h>
+#include "Sprite.h"
+
+// Class to represent text labels.
+class LabelSprite : public Sprite {
+    
+    public:
+    
+    // Factory function to control object creation.
+    static LabelSprite* GetInstance(std::string tag, std::string message, int x_pos, int y_pos);
+    
+    // Draws a static image representing the sprite.
+    virtual void Draw(int);
+    
+    virtual ~LabelSprite();
+    
+    private:
+    LabelSprite(std::string tag, std::string message, int x_pos, int y_pos); // Guard against value semantic
+    
+    LabelSprite(const LabelSprite& other_sprite); // Guard against value semantic
+    
+    const LabelSprite& operator=(const LabelSprite& other_sprite); // Guard against value semantic
+    
+    // The font.
+    TTF_Font* font;
+    
+    // The message to show.
+    std::string message;
+    
+};
+#endif
