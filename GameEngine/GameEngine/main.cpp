984485e72bca7e87275043b2f02f742885fff60e
Peter Bergman <peter.bergman@live.se>
2015-01-06 23:15:59 +0100

    Game updates

--- GameEngine/GameEngine/main.cpp
+++ GameEngine/GameEngine/main.cpp
@@ -9,15 +9,23 @@ using namespace std;
 Engine* game_engine = new Engine("SpaceShooter", 60, 800, 640);
 Level* level1 = new Level(5);
 Level* level2 = new Level(10);
-Sprite* player = AnimatedSprite::GetInstance("player", {"/Users/Peter/Documents/DSV/Prog3/images/space/player_space_ship1.png"},
-                                               0, 300, 520, 128, 128);
-Sprite* text_input = TextInputSprite::GetInstance("text_input", "enter your name:");
+Sprite* player = AnimatedSprite::GetInstance("player", {"/Users/Peter/Documents/DSV/Prog3/images/space/player_space_ship1.png", "/Users/Peter/Documents/DSV/Prog3/images/space/player_space_ship2.png", "/Users/Peter/Documents/DSV/Prog3/images/space/player_space_ship3.png"}, 300, 300, 515, 128, 128);
+Sprite* text_input = TextInputSprite::GetInstance("text_input", 400, 325);
+Sprite* name_input_message = LabelSprite::GetInstance("name_message", "enter your name:", 208, 290);
+Sprite* overlay = StaticSprite::GetInstance("overlay", "/Users/Peter/Documents/DSV/Prog3/images/gameengine/transparent.png", 0, 0, 0, 0);
 
 void CollisionListener(Sprite* sprite1, Sprite* sprite2) {
     if ((sprite1->GetTag() == "bullet" && sprite2->GetTag() == "enemy") || (sprite2->GetTag() == "bullet" && sprite1->GetTag() == "enemy")) {
         cout << "Collision detected!\n";
         level1->RemoveSprite(sprite1);
         level1->RemoveSprite(sprite2);
+    } else if ((sprite1->GetTag() == "player" && sprite2->GetTag() == "enemy") || (sprite2->GetTag() == "player" && sprite1->GetTag() == "enemy")) {
+        level1->RemoveSprite(sprite1);
+        level1->RemoveSprite(sprite2);
+        Sprite* overlay = StaticSprite::GetInstance("overlay", "/Users/Peter/Documents/DSV/Prog3/images/gameengine/transparent.png", 0, 0, 0, 0);
+        Sprite* game_over_message = LabelSprite::GetInstance("game_over_message", "Game Over!", 280, 290);
+        level1->AddSprite(overlay);
+        level1->AddSprite(game_over_message);
     }
 }
 
@@ -32,19 +40,20 @@ void PlayerLeftMove(SDL_Event event, Sprite* sprite) {
 void EnemyListenerLevel1() {
     int x_pos = rand() % game_engine->GetWindowWidth() + 100;
     if (x_pos < (game_engine->GetWindowWidth() - 100)) {
-        int delay = rand() % 50 + 15;
         Sprite* tmpSprite = MovingSprite::GetInstance("enemy" ,"/Users/Peter/Documents/DSV/Prog3/images/space/level1_enemy.png", x_pos, 0, 100, 100, 0, 1);
         level1->AddSprite(tmpSprite);
     }
 }
 
 void ShootListenerLevel1() {
-    int x_pos = player->GetX();
-    Sprite* tmpSprite = MovingSprite::GetInstance("bullet", "/Users/Peter/Documents/DSV/Prog3/images/space/level1_bullet.png", x_pos, 520, 19, 43, 0, -10);
+    int x_pos = player->GetX()+54;
+    Sprite* tmpSprite = MovingSprite::GetInstance("bullet", "/Users/Peter/Documents/DSV/Prog3/images/space/level1_bullet.png", x_pos, 505, 19, 43, 0, -10);
     level1->AddSprite(tmpSprite);
 }
 
 void PlayerNameEnteredListener() {
+    level1->RemoveSprite(overlay);
+    level1->RemoveSprite(name_input_message);
     level1->RemoveSprite(text_input);
     player->AddEventListener(PlayerRightMove, SDLK_RIGHT);
     player->AddEventListener(PlayerLeftMove, SDLK_LEFT);
@@ -60,7 +69,9 @@ int main(int argc, const char * argv[]) {
     srand(time(NULL));
     
     level1->SetBackground("/Users/Peter/Documents/DSV/Prog3/images/space/level1_background.png");
+    level1->AddSprite(overlay);
     level1->AddSprite(text_input);
+    level1->AddSprite(name_input_message);
     game_engine->AddEventListener(PlayerNameEnteredListener, SDLK_RETURN);
 
     
